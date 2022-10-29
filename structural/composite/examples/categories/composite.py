fbd57193578888e7b9e1e56c9edf677bdd2ecce3
Git Khajiit <68961533+victor-arutiunov@users.noreply.github.com>
2022-10-29 14:28:01 +0300

    composite improvements

--- structural/composite/examples/categories/composite.py
+++ structural/composite/examples/categories/composite.py
@@ -1,26 +1,79 @@
+from __future__ import annotations
 from abc import ABCMeta, abstractmethod
-from unicodedata import category
 
 
-class Component(metaclass=ABCMeta):
-
+class Composite(metaclass=ABCMeta):
     name: str
+    parent: str | None = None
 
-    @abstractmethod
-    def get_name(self, item):
-        pass
+    def __init__(self, name: str = ""):
+        self.name = name
+
+class Category(Composite):
+    children: list = []
+
+    @property
+    def components(self) -> list:
+        component:  list = self.children
+        return component
+
+    @property
+    def items(self) -> list:
+        items: list = [item for item
+                       in self.children
+                       if item.__class__ is Item]
+        return items
+
+    @property
+    def subcategories(self) -> list:
+        subcategories: list = [subcategory for subcategory
+                               in self.children
+                               if subcategory.__class__ is Category]
+        return subcategories
+
+    def _get_by_type(self, name: str, composite_type: type) -> Category | Item:
+        components: list[Item | Category] = [component for component
+                                             in self.children
+                                             if component.__class__ is composite_type
+                                             and component.name == name]
+
+        if (len(components) == 0):
+            return None
+
+        component: Composite = components[0]
+        return component
+
+    def get_item(self, name: str) -> Item | None:
+        item: Item | None = self._get_by_type(name, Item)
+        return item
+
+    def get_subcategory(self, name: str) -> Category | None:
+        subcategory: Category | None = self._get_by_type(name, Category)
+        return subcategory
+
+    def add_item(self, name: str) -> None:
+        item: Item = Item(name)
+        item.parent = self
+        self.children.append(item)
+
+    def add_subcategory(self, name: str) -> None:
+        subcategory: subcategory = Category(name)
+        subcategory.parent = self
+        self.children.append(subcategory)
 
 
-class Category(Component):
-    
-    def get_name(self):
-        return self.name
+
+class Item(Composite):
+    pass
 
 
-class Item(Component):
+category = Category("main")
 
-    def get_name(self):
-        return self.name
+items: list = ['name11', 'name5', 'name5']
+subcategories: list = ['subcat11', 'subcat5', 'subcat5']
 
+for item in items:
+    category.add_item(item)
 
-category = Category()
+for subcategory in subcategories:
+    category.add_subcategory(subcategory)
